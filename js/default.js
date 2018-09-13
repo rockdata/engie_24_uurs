4a62a2b9db24250d41ad9e09bf96d3b9eed6d83c
Srdjan Dejanovic <s.dejanovic@vegaitsourcing.rs>
2018-09-13 17:31:21 +0200

    Styles for old IE.

--- js/default.js
+++ js/default.js
@@ -10,6 +10,11 @@ let app = {
 
 		pages.kaart.initSlider();
 
+		if((navigator.userAgent.indexOf("MSIE") != -1 ) || (!!document.documentMode == true )) //IF IE > 10
+	    {
+	      	$("body").addClass("ie");
+	    }  
+
 		if(('ontouchstart' in window || navigator.msMaxTouchPoints > 0) && window.matchMedia('screen and (max-width: 1024px)').matches) {
 			$('html').addClass('touch');
 		} else {
@@ -17,7 +22,7 @@ let app = {
 		}
 	},
 	winLoad: function () {
-		
+
 	},
 	winResize: function () {
 		
--- js/dist/global.js
+++ js/dist/global.js
@@ -60,6 +60,11 @@ x();"bottom"===a.start?(c.css({top:b.outerHeight()-c.outerHeight()}),n(0,!0)):"t
 
                 pages.kaart.initSlider();
 
+                if (navigator.userAgent.indexOf("MSIE") != -1 || !!document.documentMode == true) //IF IE > 10
+                    {
+                        $("body").addClass("ie");
+                    }
+
                 if (('ontouchstart' in window || navigator.msMaxTouchPoints > 0) && window.matchMedia('screen and (max-width: 1024px)').matches) {
                     $('html').addClass('touch');
                 } else {
